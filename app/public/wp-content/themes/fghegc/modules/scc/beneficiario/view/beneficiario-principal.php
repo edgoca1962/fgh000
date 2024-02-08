0d26729d3941deae70f1e72153c580bd9d93f59b
Edwin Gonzalez <edgoca1962@gmail.com>
2024-02-08 16:48:06 -0600

    Labels captura beneficiarios

--- app/public/wp-content/themes/fghegc/modules/scc/beneficiario/view/beneficiario-principal.php
+++ app/public/wp-content/themes/fghegc/modules/scc/beneficiario/view/beneficiario-principal.php
@@ -26,18 +26,21 @@ $divpolcri = BeneficiarioController::get_instance();
          </div><!-- Foto beneficiario -->
          <div class="form-group row">
             <div class="col-md-4 mb-3">
+               <label class="form-label">Nombre</label>
                <input type="text" name="nombre" placeholder="Nombre" class="form-control" required>
                <div class="invalid-feedback">
                   Por favor indicar un nombre.
                </div>
             </div>
             <div class="col-md-4 mb-3">
+               <label class="form-label">Primer Apellido</label>
                <input type="text" name="p_apellido" placeholder="Primer Apellido" class="form-control" required>
                <div class="invalid-feedback">
                   Por favor indicar un apellido.
                </div>
             </div>
             <div class="col-md-4 mb-3">
+               <label class="form-label">Segundo Apellido</label>
                <input type="text" name="s_apellido" placeholder="Segundo Apellido" class="form-control" required>
                <div class="invalid-feedback">
                   Por favor indicar un apellido.
@@ -46,37 +49,41 @@ $divpolcri = BeneficiarioController::get_instance();
          </div><!-- Nombre y apellidos -->
          <div class="form-group row">
             <div class="col-md-4 mb-3">
-               <label for="f_nacimiento" class="form-label">Fecha Nacimiento</label>
+               <label class="form-label">Fecha Nacimiento</label>
                <input type="date" name="f_nacimiento" placeholder="Fecha de Nacimiento" class="form-control" id="f_nacimiento" required>
                <div class="invalid-feedback">
                   Por favor indicar la fecha de nacimiento.
                </div>
             </div>
             <div class="col-md-4 mb-3">
-               <label for="f_ingreso" class="form-label">Fecha Ingreso</label>
+               <label class="form-label">Fecha Ingreso</label>
                <input type="date" name="f_ingreso" placeholder="Fecha de Ingreso" class="form-control" required>
                <div class="invalid-feedback">
                   Por favor indicar la fecha de ingreso.
                </div>
             </div>
             <div class="col-md-4 mb-3">
-               <label for="" class="form-label">Fecha Salida</label>
+               <label class="form-label">Fecha Salida</label>
                <input type="date" name="f_salida" placeholder="Fecha de Salida" class="form-control">
             </div>
          </div><!-- fecha nacimiento, ingreso y salida -->
          <div class="form-group row">
             <div class="col-md-4 mb-3">
+               <label class="form-label">Edad</label>
                <input type="text" name="edad" placeholder="Edad (este dato se calcula)" class="form-control" id="edad" disabled>
             </div>
             <div class="col-md-4 mb-3">
+               <label class="form-label">Peso (kg)</label>
                <input type="text" name="peso" placeholder="Peso en kilos" class="form-control">
             </div>
             <div class="col-md-4 mb-3">
+               <label class="form-label">Estatura (m)</label>
                <input type="text" name="estatura" placeholder="Estatura en metros" class="form-control">
             </div>
          </div><!-- Edad, peso y estatura -->
          <div class="form-group row">
             <div class="col-md-4 mb-3">
+               <label class="form-label">Provincia</label>
                <select id="provincia" name="provincia" class="form-select" aria-label="Provincias">
                   <option selected>Seleccionar Provincia</option>
                   <?php foreach ($divpolcri->get_provincias() as $provincia) : ?>
@@ -85,6 +92,7 @@ $divpolcri = BeneficiarioController::get_instance();
                </select>
             </div>
             <div class="col-md-4 mb-3">
+               <label class="form-label">Cantón</label>
                <select id="canton" name="canton" class="form-select" aria-label="Cantones">
                   <option selected>Seleccionar Cantón</option>
                </select>
@@ -92,6 +100,7 @@ $divpolcri = BeneficiarioController::get_instance();
                <input id="action_canton" type="hidden" name="action_canton" value="canton">
             </div>
             <div class="col-md-4 mb-3">
+               <label class="form-label">Distrito</label>
                <select id="distrito" name="distrito" class="form-select" aria-label="Distritos">
                   <option selected>Seleccionar Distrito</option>
                </select>
@@ -100,33 +109,39 @@ $divpolcri = BeneficiarioController::get_instance();
             </div>
          </div><!-- Provincia, Cantón y Distrito -->
          <div class="form-group mb-3">
+            <label class="form-label">Detalle de dirección</label>
             <input name="direccion" class="form-control" placeholder="Espacio para detallar la dirección"></input>
          </div><!-- dirección detallada -->
          <div class="form-group row">
             <div class="col-md-4 mb-3">
+               <label class="form-label">e-mail</label>
                <input type="email" name="email" placeholder="e-mail" class="form-control" required>
                <div class="invalid-feedback">
                   Por favor indicar un correo válido.
                </div>
             </div>
             <div class="col-md-4 mb-3">
+               <label class="form-label">Teléfono Principal</label>
                <input type="text" name="t_principal" placeholder="Teléfono principal" class="form-control" required>
                <div class="invalid-feedback">
                   Por favor indicar un teléfono móvil.
                </div>
             </div>
             <div class="col-md-4 mb-3">
+               <label class="form-label">Otros Teléfonos</label>
                <input type="text" name="t_otros" placeholder="Otros Teléfonos" class="form-control">
             </div>
          </div><!-- correo y teléfono -->
          <div class="form-group row">
             <div class="col-md-4 mb-3">
-               <input type="text" name="n_madre" placeholder="Nombre de la madre o tutor" class="form-control" required>
+               <label class="form-label">Nombre de la Madre o Tutor(a)</label>
+               <input type="text" name="n_madre" placeholder="Nombre de la madre o tutor(a)" class="form-control" required>
                <div class="invalid-feedback">
                   Por favor indicar un nombre de la madre o tutor.
                </div>
             </div>
             <div class="col-md-4 mb-3">
+               <label class="form-label">Nombre del Padre</label>
                <input type="text" name="n_padre" placeholder="Nombre del padre" class="form-control" required>
                <div class="invalid-feedback">
                   Por favor indicar un nombre del padre.
@@ -149,7 +164,7 @@ $divpolcri = BeneficiarioController::get_instance();
       </form>
    </div>
    <div class="col-md-4">
-      <?php get_template_part('sidebar')
+      <?php get_template_part($atributos['sidebar'])
       ?>
    </div>
 </div>
